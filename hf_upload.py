445f47b019e6e8ed4b92f9d15c2ce5619b34be3d
jacob-papermill <jacob@papermill.io>
2025-10-31 10:49:59 +0000

    Renamed export path

--- hf_upload.py
+++ hf_upload.py
@@ -4,20 +4,10 @@ from torch import nn
 from huggingface_hub import PyTorchModelHubMixin
 from dotenv import load_dotenv
 
-from src.u2net import U2NET as BaseModel
+from src.u2net import U2NET
 
 
 load_dotenv()
-
-
-class U2NET(nn.Module, PyTorchModelHubMixin):
-    def __init__(self, in_ch: int, out_ch: int):
-        super(U2NET, self).__init__()
-
-        self.model = BaseModel(in_ch, out_ch)
-
-    def forward(self, x):
-        self.model(x)
 
 
 config = {
