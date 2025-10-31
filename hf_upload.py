90998d59d7f05cd5589c3ae14f69c5bf4e33de1a
jacob-papermill <jacob@papermill.io>
2025-10-31 10:40:13 +0000

    Added huggingface upload script

---
+++ hf_upload.py
@@ -0,0 +1,31 @@
+import os
+
+from torch import nn
+from huggingface_hub import PyTorchModelHubMixin
+from dotenv import load_dotenv
+
+from model.u2net import U2NET as BaseModel
+
+
+load_dotenv()
+
+
+class U2NET(nn.Module, PyTorchModelHubMixin):
+    def __init__(self, in_ch: int, out_ch: int):
+        super(U2NET, self).__init__()
+
+        self.model = BaseModel(in_ch, out_ch)
+
+    def forward(self, x):
+        self.model(x)
+
+
+config = {
+    "in_ch": 3,
+    "out_ch": 1
+}
+
+model = U2NET(**config)
+
+model.save_pretrained("U2NET")
+model.push_to_hub("papermill/U2NET", token=os.getenv("HF_HUB_TOKEN"))
--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,15 @@
-from .u2net import U2NET
-from .u2net import U2NETP
+from torch import nn
+from huggingface_hub import PyTorchModelHubMixin
+
+from .u2net import U2NET as U2NETBase
+from .u2net import U2NETP as U2NETPBase
+
+
+class U2NET(nn.Module, PyTorchModelHubMixin):
+    def __init__(self, in_ch: int, out_ch: int):
+        super(U2NET, self).__init__()
+
+        self.model = U2NETBase(in_ch, out_ch)
+
+    def forward(self, x):
+        self.model(x)
