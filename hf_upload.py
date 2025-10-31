9b4d82b4986588780d4c740457fa93baf21bb177
jacob-papermill <jacob@papermill.io>
2025-10-31 11:34:28 +0000

    Loads checkpoint before upload

--- hf_upload.py
+++ hf_upload.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from dotenv import load_dotenv
 
 from src.u2net import U2NET
@@ -13,7 +14,14 @@ config = {
     "out_ch": 1
 }
 
+state_dict = torch.load("checkpoints/u2net.pth", map_location="cpu")
+
 model = U2NET(**config)
+model.load_state_dict(
+    {
+        f"model.{k}": v for k, v in state_dict.items()
+    }
+)
 
 model.save_pretrained("U2NET")
 model.push_to_hub("papermill/U2NET", token=os.getenv("HF_HUB_TOKEN"))
